7fc8e8bf59dd2b9e136b9f526fb13b26fc540b3f
Borislav Semerdzhiev <borislavsemerdzhiev.02@gmail.com>
2026-06-22 23:26:45 +0200

    add benchmarks

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,14 @@
 #include "render_simulation.hpp"
 #include <cstdint>
 
+constexpr bool RENDER = false;
+
 int32_t main() {
-  RenderSimulation::render();
-  // BenchmarkRun::run();
+  if constexpr (RENDER) {
+    RenderSimulation::render();
+  } else {
+    BenchmarkRun::run();
+  }
 
   return 0;
 }
